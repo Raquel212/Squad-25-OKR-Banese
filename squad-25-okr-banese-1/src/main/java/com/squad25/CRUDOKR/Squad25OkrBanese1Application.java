a5d83e7e39d19904ec7d477d60d098a616d8ea83
Murilo <Murilo@DESKTOP-032HGKH>
2024-04-06 17:26:53 -0300

    Projeto Base

---
+++ squad-25-okr-banese-1/src/main/java/com/squad25/CRUDOKR/Squad25OkrBanese1Application.java
@@ -0,0 +1,13 @@
+package com.squad25.CRUDOKR;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Squad25OkrBanese1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Squad25OkrBanese1Application.class, args);
+	}
+
+}
