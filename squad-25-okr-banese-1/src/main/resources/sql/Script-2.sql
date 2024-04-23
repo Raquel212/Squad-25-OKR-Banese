6fbef1a3aeeadcb502fa2ca82060e212871b9985
mvocarvalho <marcos.carvalho@deal.com.br>
2024-04-23 18:41:53 -0300

    BAN-X: Ajuste nos scripts

--- squad-25-okr-banese-1/src/main/resources/sql/Script-2.sql
+++ squad-25-okr-banese-1/src/main/resources/sql/Script-2.sql
@@ -1,5 +1,13 @@
+CREATE TABLE squad_25.Usuario (
+    id INT(10) AUTO_INCREMENT PRIMARY KEY,
+    login VARCHAR(100) UNIQUE,
+    senha VARCHAR(100),
+    dataCadastro DATE,
+    ativo BIT
+);
+
 CREATE TABLE squad_25.Responsavel (
-    id INT(10) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    id INT(10) AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(300),
     CONSTRAINT FK_Usuario FOREIGN KEY (id) REFERENCES Usuario(id)
 );
